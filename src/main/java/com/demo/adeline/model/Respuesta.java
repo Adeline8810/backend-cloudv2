6aaf2c332bc1c009442b85e18950988385c13018
Ruth <ruthmarilsa20@gmail.com>
2026-03-20 11:46:12 +0100

    arreglando el metodo de mierda!!!

--- src/main/java/com/demo/adeline/model/Respuesta.java
+++ src/main/java/com/demo/adeline/model/Respuesta.java
@@ -9,28 +9,62 @@ public class Respuesta {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Forzamos el nombre exacto de la columna para que Spring no adivine
+    @Column(name = "preguntaId") 
     private Long preguntaId;
+
+    @Column(name = "usuarioId")
     private Long usuarioId;
 
     @Column(columnDefinition = "TEXT")
     private String texto; 
 
+    @Column(name = "fotoUrl")
     private String fotoUrl;
 
-    public Respuesta() {}
+	public Long getId() {
+		return id;
+	}
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public Long getPreguntaId() { return preguntaId; }
-    public void setPreguntaId(Long preguntaId) { this.preguntaId = preguntaId; }
+	public Long getPreguntaId() {
+		return preguntaId;
+	}
 
-    public Long getUsuarioId() { return usuarioId; }
-    public void setUsuarioId(Long usuarioId) { this.usuarioId = usuarioId; }
+	public void setPreguntaId(Long preguntaId) {
+		this.preguntaId = preguntaId;
+	}
 
-    public String getTexto() { return texto; }
-    public void setTexto(String texto) { this.texto = texto; }
+	public Long getUsuarioId() {
+		return usuarioId;
+	}
 
-    public String getFotoUrl() { return fotoUrl; }
-    public void setFotoUrl(String fotoUrl) { this.fotoUrl = fotoUrl; }
+	public void setUsuarioId(Long usuarioId) {
+		this.usuarioId = usuarioId;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public String getFotoUrl() {
+		return fotoUrl;
+	}
+
+	public void setFotoUrl(String fotoUrl) {
+		this.fotoUrl = fotoUrl;
+	}
+
+  
+    
+    
+    
+    
 }
