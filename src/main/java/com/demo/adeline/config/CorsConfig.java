0e43b0917ad99b98d06f0c5a71bd700904867398
Ruth <ruthmarilsa20@gmail.com>
2026-03-26 23:46:48 +0100

    Subiendo mi proyecto Angular v2

--- src/main/java/com/demo/adeline/config/CorsConfig.java
+++ src/main/java/com/demo/adeline/config/CorsConfig.java
@@ -16,10 +16,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                     // IMPORTANTE: No uses "*". Pon las URLs específicas.
-                    .allowedOrigins("https://adeline8810.github.io", "http://localhost:4200", 
-                    		"http://localhost:8080",
-                    		"https://adeline8810.github.io/proyectoSlam/slam",
-                    		"https://adeline8810.github.io/proyectoSlam/","https://backend-ruth-slam.onrender.com")
+                    .allowedOrigins("*")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
