b2424ab656a108aaedbe2f77f932e9f0d7554fb3
Ruth <ruthmarilsa20@gmail.com>
2026-04-27 11:36:08 +0200

    Subiendo la parte para  recargar 2704-2

--- src/main/java/com/demo/adeline/config/CloudinaryConfig.java
+++ src/main/java/com/demo/adeline/config/CloudinaryConfig.java
@@ -9,22 +9,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class CloudinaryConfig {
 
-    @Value("${cloudinary.cloud_name}")
+    @Value("${CLOUDINARY_CLOUD_NAME}")
     private String cloudName;
 
-    @Value("${cloudinary.api_key}")
+    @Value("${CLOUDINARY_API_KEY}")
     private String apiKey;
 
-    @Value("${cloudinary.api_secret}")
+    @Value("${CLOUDINARY_API_SECRET}")
     private String apiSecret;
 
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", cloudName,
-                "api_key", apiKey,
-                "api_secret", apiSecret,
-                "secure", true
+            "cloud_name", cloudName,
+            "api_key", apiKey,
+            "api_secret", apiSecret
         ));
     }
 }
